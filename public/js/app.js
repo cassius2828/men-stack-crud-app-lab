429d7468443ec5194ec5adf4b3d842896fb17a46
Cassius <cassius.reynolds.dev@gmail.com>
2024-06-04 21:19:38 -0700

    improved the responsiveness of the canvas container and title when opening the side menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,15 @@
 const hamburger = document.querySelector(".hamburger-container");
 const bar = document.querySelector(".bar");
 const sidebar = document.querySelector('.sidebar')
+const canvasContainer = document.querySelector('.canvas-container')
+const galleriesTitle = document.querySelector('.galleries-title')
+
 console.log(hamburger)
 hamburger.addEventListener('click', (e) => {
     bar.classList.toggle('close');
     sidebar.classList.toggle('hide')
+    canvasContainer.classList.toggle('full')
+    galleriesTitle.classList.toggle('full')
     console.log('closed')
     console.log(e.target)
 })
